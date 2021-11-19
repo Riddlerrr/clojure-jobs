1d56c30498dc84b82b8fdb041b737a84b22bcee3
Sergey Andronov <riddlerrr@gmail.com>
2021-11-19 11:34:13 +0100

    Fix time-ago

--- src/clj/clojure_jobs_luminus/util.clj
+++ src/clj/clojure_jobs_luminus/util.clj
@@ -5,7 +5,7 @@
 (defn time-ago [from-time]
   (let [units [{:name "сек." :limit 60 :in-second 1}
                {:name "мин." :limit 3600 :in-second 60}
-               {:name "час." :limit 86400 :in-second 3600}
+               {:name "ч." :limit 86400 :in-second 3600}
                {:name "д." :limit 604800 :in-second 86400}
                {:name "нед." :limit 2629743 :in-second 604800}
                {:name "мес." :limit 31556926 :in-second 2629743}
