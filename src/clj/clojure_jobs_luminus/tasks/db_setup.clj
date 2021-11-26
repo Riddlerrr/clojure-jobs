8a323bc47e02c522a79073e5fd65a66bc460311f
Sergey Andronov <riddlerrr@gmail.com>
2021-11-26 10:51:55 +0100

    Fiix creating db

--- src/clj/clojure_jobs_luminus/tasks/db_setup.clj
+++ src/clj/clojure_jobs_luminus/tasks/db_setup.clj
@@ -11,27 +11,27 @@
    #'clojure-jobs-luminus.config/env
    #'clojure-jobs-luminus.db.core/*db*))
 
+(def init-db {:dbtype "postgresql" :dbname "postgres"})
+(def init-ds (jdbc/get-datasource init-db))
+
 (defn create-db [db-name]
   (let [db-exists-sql "select datname FROM pg_catalog.pg_database WHERE datname=?"
         create-sql (str "create database " db-name " with encoding 'utf8'")
-        db-exists? (:pg_database/datname (jdbc/execute-one! *db* [db-exists-sql db-name]))]
+        db-exists? (:pg_database/datname (jdbc/execute-one! init-ds [db-exists-sql db-name]))]
     (if db-exists?
       (println "Database" db-name "already exists, skipping creation")
       (do
         (println "Creating database: " db-name)
-        (jdbc/execute! *db* [create-sql])
-        ))))
+        (jdbc/execute! init-ds [create-sql])))))
 
 (defn migrate-db []
   (migrations/migrate ["migrate"] (select-keys env [:database-url])))
 
 (defn -main []
-  (connect)
   (create-db "clojure_jobs_luminus_test")
+  (connect)
   (migrate-db)
-  (System/exit 0)
-)
+  (System/exit 0))
 
 (comment
-  (create-db "clojure_jobs_luminus_test")
-)
+  (create-db "clojure_jobs_luminus_test"))
